da176538b002317e2837a260e2739a9c3e892db6
Ethan Christensen <etc@unknownstudio.com>
2024-09-25 11:31:39 -0600

    Added text-case; Changed vim-abolish keybinds
    Using text-case for case coercion now. Keeping vim-abolish for its
    Subvert(Su/S) and Abolish commands

--- lua/plugins/vim-abolish.lua
+++ lua/plugins/vim-abolish.lua
@@ -1,12 +1,4 @@
 return {
   "tpope/vim-abolish",
-  keys = {
-    -- Ensure that typing `c` in normal mode will trigger the `Coerce Case` prompt.
-    -- `c` usually enters "o" mode (operator-pending)
-    { "r", "<Plug>(ABOLISH-coerce-word)", mode = "o", desc = "Coerce Case" },
-    -- This is the default keybind but it still is needed for some reason I think
-    { "cr", "<Plug>(abolish-coerce-word)", mode = "n", desc = "Coerce Case" },
-    -- Coerce in visual mode
-    { "cr", "<Plug>(abolish-coerce)", mode = "v", desc = "Coerce Case" },
-  },
+  cmd = { "Abolish", "Subvert", "S" },
 }
