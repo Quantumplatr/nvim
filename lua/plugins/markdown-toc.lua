d3a9c3b854e68479f28c696fc59390f4c239b86c
Ethan Christensen <etc@unknownstudio.com>
2024-10-10 23:20:44 -0600

    fix(toc): swap nvim-toc for markdown-toc
    nvim-toc didn't seem to work

---
+++ lua/plugins/markdown-toc.lua
@@ -0,0 +1,9 @@
+-- Ref: https://github.com/hedyhli/markdown-toc.nvim
+return {
+  "hedyhli/markdown-toc.nvim",
+  -- ft = "markdown", -- Lazy load on markdown filetype
+  cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
+  opts = {
+    -- Your configuration here (optional)
+  },
+}
