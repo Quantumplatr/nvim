8f7a1bfa4ce0b2db0874017d7566f340a6db5e44
Ethan Christensen <etc@unknownstudio.com>
2024-11-25 13:46:55 -0700

    feat(dashboard): update dashboard and switch to smear-cursor
    LazyVim switched to snacks.nvim for a dashboard so this updates
    accordingly. I also switched to smear-cursor from LazyExtra's
    mini-animate as I think it looks better.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,97 +1,48 @@
--- LazyVim start: https://www.lazyvim.org/plugins/ui#dashboard-nvim
--- Eldritch: https://github.com/eldritch-theme/eldritch.nvim?tab=readme-ov-file#extras
+-- Ref: https://github.com/folke/snacks.nvim/blob/main/docs/dashboard.md
 
 return {
   {
-
-    "nvimdev/dashboard-nvim",
-
-    ---------------------
-    --- LazyVim Start ---
-    ---------------------
-
-    opts = function()
-      local logo = [[
-⠀⠀⠀⠀⠀⠀⠀⣠⡤⠶⡄⠀⠀⠀⠀⠀⠀⠀⢠⠶⣦⣀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢀⣴⣿⡟⠀⠈⣀⣾⣝⣯⣿⣛⣷⣦⡀⠀⠈⢿⣿⣦⡀⠀⠀⠀⠀
-⠀⠀⠀⣴⣿⣿⣿⡇⠀⢼⣿⣽⣿⢻⣿⣻⣿⣟⣷⡄⠀⢸⣿⣿⣾⣄⠀⠀⠀
-⠀⠀⣞⣿⣿⣿⣿⣷⣤⣸⣟⣿⣿⣻⣯⣿⣿⣿⣿⣀⣴⣿⣿⣿⣿⣯⣆⠀⠀
-⠀⡼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣜⡆⠀
-⢠⣟⣯⣿⣿⣿⣷⢿⣫⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣟⠿⣿⣿⣿⣿⡷⣾⠀
-⢸⣯⣿⣿⡏⠙⡇⣾⣟⣿⡿⢿⣿⣿⣿⣿⣿⢿⣟⡿⣿⠀⡟⠉⢹⣿⣿⢿⡄
-⢸⣯⡿⢿⠀⠀⠱⢈⣿⢿⣿⡿⣏⣿⣿⣿⣿⣿⣿⣿⣿⣀⠃⠀⢸⡿⣿⣿⡇
-⢸⣿⣇⠈⢃⣴⠟⠛⢉⣸⣇⣹⣿⣿⠚⡿⣿⣉⣿⠃⠈⠙⢻⡄⠎⠀⣿⡷⠃
-⠈⡇⣿⠀⠀⠻⣤⠠⣿⠉⢻⡟⢷⣝⣷⠉⣿⢿⡻⣃⢀⢤⢀⡏⠀⢠⡏⡼⠀
-⠀⠘⠘⡅⠀⣔⠚⢀⣉⣻⡾⢡⡾⣻⣧⡾⢃⣈⣳⢧⡘⠤⠞⠁⠀⡼⠁⠀⠀
-⠀⠀⠀⠸⡀⠀⢠⡎⣝⠉⢰⠾⠿⢯⡘⢧⡧⠄⠀⡄⢻⠀⠀⠀⢰⠁⠀⠀⠀
-⠀⠀⠀⠀⠁⠀⠈⢧⣈⠀⠘⢦⠀⣀⠇⣼⠃⠰⣄⣡⠞⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⢤⠼⠁⠀⠀⠳⣤⡼⠀⠀⠀⠀⠀⠀
-    ]]
-      logo = string.rep("\n", 2) .. logo
-
-      --   local logo = [[
-      --      ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-      --      ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
-      --      ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
-      --      ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
-      --      ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
-      --      ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
-      -- ]]
-      --
-      --   logo = string.rep("\n", 8) .. logo .. "\n\n"
-
-      local opts = {
-        theme = "doom",
-        hide = {
-          -- this is taken care of by lualine
-          -- enabling this messes up the actual laststatus setting after loading a file
-          statusline = false,
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░        ░░        ░░        ░░  ░░░░  ░░        ░░  ░░░░  ░
+▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒▒▒▒  ▒▒▒▒▒   ▒▒   ▒
+▓      ▓▓▓▓      ▓▓▓▓      ▓▓▓▓▓  ▓▓  ▓▓▓▓▓▓  ▓▓▓▓▓        ▓
+█  ████████  ████████  ██████████    ███████  █████  █  █  █
+█        ██        ██  ███████████  █████        ██  ████  █
+████████████████████████████████████████████████████████████
+]],
         },
-        config = {
-          header = vim.split(logo, "\n"),
-      -- stylua: ignore
-      center = {
-        { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
-        { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-        { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-        { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
-        { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
-        { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-        { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
-        { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
-        { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+        sections = {
+          -- Header
+          { section = "header" },
+          -- Keymaps
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          -- Recent Files
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          -- Projects
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          -- Git
+          {
+            icon = "",
+            title = "Git Status",
+            section = "terminal",
+            -- enabled = Snacks.git.get_root() ~= nil, TODO: figure out why this breaks
+            cmd = "hub status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          -- Startup
+          { section = "startup" },
+        },
       },
-          footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-          end,
-        },
-      }
-
-      for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        button.key_format = "  %s"
-      end
-
-      -- open dashboard after closing lazy
-      if vim.o.filetype == "lazy" then
-        vim.api.nvim_create_autocmd("WinClosed", {
-          pattern = tostring(vim.api.nvim_get_current_win()),
-          once = true,
-          callback = function()
-            vim.schedule(function()
-              vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
-            end)
-          end,
-        })
-      end
-
-      return opts
-    end,
-
-    -------------------
-    --- LazyVim End ---
-    -------------------
+    },
   },
 }
