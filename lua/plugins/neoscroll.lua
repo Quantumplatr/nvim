ed7c7c003fcae551b281fe920429816e7cfc33ff
Ethan Christensen <etc@unknownstudio.com>
2024-12-15 18:31:11 -0700

    feat(neoscroll): disable neoscroll

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,11 +1,13 @@
 return {
-  {
-    "karb94/neoscroll.nvim",
-    config = function()
-      require("neoscroll").setup({
-        hide_cursor = false,
-        easing = "quartic",
-      })
-    end,
-  },
+  -- NOTE: LazyVim update added some animation stuff I believe. This seemed to be conflicting.
+  -- and caused some weird scrolling behavior. Keeping it commented out incase I need it later.
+  --   {
+  --     "karb94/neoscroll.nvim",
+  --     config = function()
+  --       require("neoscroll").setup({
+  --         hide_cursor = false,
+  --         easing = "quartic",
+  --       })
+  --     end,
+  --   },
 }
